ab1b8005aa14ba3f0dbb4bebd4670786f01b1ff7
Steven Hans Limantoro <40755531+StevenWuzz@users.noreply.github.com>
2025-12-05 15:01:29 +0700

    Deleted unnecessary target directory

--- task1-StevenWuzz/target/debug/deps/task1-b89b1238ee28c76b.d
+++
@@ -1,5 +0,0 @@
-/Users/stevenhanslimantoro/task1-StevenWuzz/target/debug/deps/libtask1-b89b1238ee28c76b.rmeta: src/main.rs
-
-/Users/stevenhanslimantoro/task1-StevenWuzz/target/debug/deps/task1-b89b1238ee28c76b.d: src/main.rs
-
-src/main.rs:
--- task1-StevenWuzz/target/debug/deps/task1-ef224575e277db32.d
+++
@@ -1,5 +0,0 @@
-/Users/stevenhanslimantoro/task1-StevenWuzz/target/debug/deps/libtask1-ef224575e277db32.rmeta: src/main.rs
-
-/Users/stevenhanslimantoro/task1-StevenWuzz/target/debug/deps/task1-ef224575e277db32.d: src/main.rs
-
-src/main.rs:
